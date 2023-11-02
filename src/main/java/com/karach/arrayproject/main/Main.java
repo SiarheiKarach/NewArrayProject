f9d60f84dea3a72c076dcab90916bc3e1fa38cb1
skarach <karach19071992@gmail.com>
2023-11-02 14:05:19 +0100

    Change 1

--- src/main/java/com/karach/arrayproject/main/Main.java
+++ src/main/java/com/karach/arrayproject/main/Main.java
@@ -1,64 +1,35 @@
 package com.karach.arrayproject.main;
-
 import com.karach.arrayproject.exception.ArrayModelException;
-import com.karach.arrayproject.logging.Logger;
 import com.karach.arrayproject.readfile.ReadFile;
+import com.karach.arrayproject.readfile.impl.ReadFileImpl;
 import com.karach.arrayproject.service.ArrayModelBubbleSort;
 import com.karach.arrayproject.service.ArrayModelService;
+import com.karach.arrayproject.parser.NumberParser;
 import com.karach.arrayproject.service.impl.ArrayModelServiceImpl;
-
-import java.io.IOException;
+import com.karach.arrayproject.validation.ArrayValidation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-/*    int[] myArray = new int[10];
-    Random random = new Random();
-    int i = 0;
-    for (int element : myArray) {
-      myArray[i] = random.nextInt(-5, 2);
-      i++;
-    }
-    ArrayModelService arrayModelService = new ArrayModelServiceImpl();
-    ArrayModelBubbleSort arrayModelBubbleSort = new ArrayModelBubbleSort();
-    arrayModelBubbleSort.bubbleSort(myArray);
-    //Arrays.sort(myArray);
-
+  public static void main(String[] args) {
+    Logger logger = LogManager.getLogger();
+    ReadFile readFile = new ReadFileImpl();
     try {
-      int min = arrayModelService.findMin(myArray);
-      int max = arrayModelService.findMax(myArray);
-      int replacedCount = arrayModelService.replaceElementsNumber(myArray, 2, 6);
-      double average = arrayModelService.findAverage(myArray);
-      int sum = arrayModelService.findSum(myArray);
-      int positiveCount = arrayModelService.countPositiveElements(myArray);
-      int negativeCount = arrayModelService.countNegativeElements(myArray);
+      InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("myArray.txt");
+      List<String> newArrayFromFile = new BufferedReader(new InputStreamReader(inputStream))
+              .lines().collect(Collectors.toList());
 
-      Logger.logInfo("Отсортированный массив: " + Arrays.toString(myArray));
-      Logger.logInfo("Минимальное значение массива:" + min);
-      Logger.logInfo("Максимальное значение массива:" + max);
-      Logger.logInfo("Измененное число элементов массива:" + replacedCount);
-      Logger.logInfo("Среднее значение элементов массива: " + average);
-      Logger.logInfo("Cумма элементов массива: " + sum);
-      Logger.logInfo("Cумма положительных элементов массива: " + positiveCount);
-      Logger.logInfo("Cумма отрицательных элементов массива: " + negativeCount);
-
-    } catch (ArrayModelException e) {
-      Logger.logError("Ошибка: " + e.getMessage());
-    }*/
-    String filePath = ".\\src\\main\\resources\\myArray.txt";
-    List<String> newArrayFromFile = ReadFile.readLines(filePath);
-
-
-    int[] newArray = newArrayFromFile.stream()
-            .mapToInt(Integer::parseInt)
-            .toArray();
-    ArrayModelService arrayModelService = new ArrayModelServiceImpl();
-    ArrayModelBubbleSort arrayModelBubbleSort = new ArrayModelBubbleSort();
-    arrayModelBubbleSort.bubbleSort(newArray);
-    try {
+      int[] newArray = NumberParser.parseStringListToIntArray(newArrayFromFile);
+      if (ArrayValidation.isArrayNotEmpty(newArray) && !ArrayValidation.isNegativeNumber(newArray)) {
+      ArrayModelService arrayModelService = new ArrayModelServiceImpl();
+      ArrayModelBubbleSort arrayModelBubbleSort = new ArrayModelBubbleSort();
+      arrayModelBubbleSort.bubbleSort(newArray);
 
       int min = arrayModelService.findMin(newArray);
       int max = arrayModelService.findMax(newArray);
@@ -68,16 +39,18 @@ public class Main {
       int positiveCount = arrayModelService.countPositiveElements(newArray);
       int negativeCount = arrayModelService.countNegativeElements(newArray);
 
-      Logger.logInfo("Отсортированный массив: " + Arrays.toString(newArray));
-      Logger.logInfo("Минимальное значение массива:" + min);
-      Logger.logInfo("Максимальное значение массива:" + max);
-      Logger.logInfo("Измененное число элементов массива:" + replacedCount);
-      Logger.logInfo("Среднее значение элементов массива: " + average);
-      Logger.logInfo("Cумма элементов массива: " + sum);
-      Logger.logInfo("Cумма положительных элементов массива: " + positiveCount);
-      Logger.logInfo("Cумма отрицательных элементов массива: " + negativeCount);
+      logger.info("Sorted array: " + Arrays.toString(newArray));
+      logger.info("Minimal array value: " + min);
+      logger.info("Maximal array value: " + max);
+      logger.info("Number of changed elements in array: " + replacedCount);
+      logger.info("Average value of array: " + average);
+      logger.info("Sum of all elements in array: " + sum);
+      logger.info("Sum of positive elements in array: " + positiveCount);
+      logger.info("Sum of negative elements in array: " + negativeCount);
+      } else {
+        logger.error("Array is empty or contains negative numbers.");}
     } catch (ArrayModelException e) {
-      Logger.logError("Ошибка в работе с массивом: " + e.getMessage());
+      logger.error("Error: " + e.getMessage());
     }
   }
 }
